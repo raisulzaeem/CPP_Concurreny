c7fdf84b3648b426d7dfe25adad64b312741ff4a
raisulzaeem <raisulzaeem@gmail.com>
2023-03-06 11:03:29 +0100

    Comments on passing exception from the thread

--- Passing_data_between_threads/future_passing_exception.cpp
+++ Passing_data_between_threads/future_passing_exception.cpp
@@ -15,12 +15,15 @@ void divideByNumber(std::promise<double> &&prms, double num, double denom)
     try
     {
         if (denom == 0)
+        // in stead of setting a value, we now want to throw a std::exception along with a customized error message
             throw std::runtime_error("Exception from thread: Division by zero!");
         else
             prms.set_value(num / denom);
     }
     catch (...)
     {
+        // The function std::current_exception allows us to easily retrieve the exception which has been thrown.
+        // we catch this exception and throw it to the parent thread using the promise with set_exception.
         prms.set_exception(std::current_exception());
     }
 }
@@ -36,13 +39,17 @@ int main()
     std::thread t(divideByNumber, std::move(prms), num, denom);
 
     // retrieve result within try-catch-block
+    // On the parent side, we now need to catch this exception. In order to do this, we can use a try-block around the call to get(). 
     try
     {
         double result = ftr.get();
         std::cout << "Result = " << result << std::endl;
     }
+    // We can set the catch-block to catch all exceptions or - as in this example - 
+    // we could also catch a particular one such as the standard exception. 
     catch (std::runtime_error e)
     {
+        // Calling the method what() on the exception allows us to retrieve the message from the exception.
         std::cout << e.what() << std::endl;
     }
 
