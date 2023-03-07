1df07d072d81a93f0da99479d58a4e21d7ea2bcc
raisulzaeem <raisulzaeem@gmail.com>
2023-03-08 00:41:31 +0100

    Solution to L2 tasks

--- L2_Project/src/Intersection.cpp
+++ L2_Project/src/Intersection.cpp
@@ -26,6 +26,10 @@ void WaitingVehicles::permitEntryToFirstInQueue()
     // L2.3 : First, get the entries from the front of _promises and _vehicles. 
     // Then, fulfill promise and send signal back that permission to enter has been granted.
     // Finally, remove the front elements from both queues. 
+    _promises.at(0).set_value();
+
+    _vehicles.erase(_vehicles.begin());
+    _promises.erase(_promises.begin());
 }
 
 /* Implementation of class "Intersection" */
@@ -63,6 +67,10 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // L2.2 : First, add the new vehicle to the waiting line by creating a promise, a corresponding future and then adding both to _waitingVehicles. 
     // Then, wait until the vehicle has been granted entry. 
+    std::promise<void> prms;
+    std::future<void> ftr = prms.get_future();
+    _waitingVehicles.pushBack(vehicle, std::move(prms));
+    ftr.wait();
 
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
